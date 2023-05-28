50825b6975fa251213e20ef6bc2ac6cc4a7a840f
Adriano <adriano_alves_2008@hotmail.com>
2023-05-28 11:44:08 -0300

    criando estilos globais e de formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Global } from './styles/global'
+import styled from 'styled-components'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Form from './components/Form'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2``
+
 function App() {
   return (
-    <div>
-      <h1>teste</h1>
-    </div>
+    <Container>
+      <Title>USUÁRIOS</Title>
+      <Form />
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <Global />
+    </Container>
   )
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Global = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #f2f2f2;
+  }
+`
